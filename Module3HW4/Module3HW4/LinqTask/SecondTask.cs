3fb33b71f3ca22a014d91d26d0f8f0b3a4027daa
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-16 21:20:25 +0200

    added Linq task

---
+++ Module3HW4/Module3HW4/LinqTask/SecondTask.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Module3HW4.LinqTask
+{
+    public class SecondTask
+    {
+        public void CallLinqMethods(List<Contact> list)
+        {
+            var firstOrDefaultElement = list.FirstOrDefault();
+            var youngContacts = list.Where(w => w.Age < 25);
+            var modifiedNumbers = list.Select(s => s.PhoneNumber.Insert(0, "+"));
+            var amountOfOlderContacts = list.Count(c => c.Age > 25);
+            var sortedContacts = list.OrderBy(o => o.Age).ThenBy(t => t.FirstName);
+            var averageAge = list.Average(a => a.Age);
+            var groupedElements = list.Take(3);
+        }
+    }
+}
---
+++ Module3HW4/Module3HW4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Module3HW4.DelegateTask;
+using Module3HW4.LinqTask;
+
+namespace Module3HW4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var program = new Program();
+            program.DoDelegateTask();
+            program.DoLinqTask();
+        }
+
+        public void DoDelegateTask()
+        {
+            var firstTask = new FirstTask();
+            var sum = 0D;
+            firstTask.TryCatch(() => sum = firstTask.CountSum(7, 7));
+            Console.WriteLine(sum);
+        }
+
+        public void DoLinqTask()
+        {
+            var secondTask = new SecondTask();
+            var list = new List<Contact>();
+            list.Add(new Contact() { FirstName = "Neil", LastName = "Binkley", Age = 34, PhoneNumber = "972-671-0874" });
+            list.Add(new Contact() { FirstName = "Lauren", LastName = "Cuevas", Age = 19, PhoneNumber = "610-235-7220" });
+            list.Add(new Contact() { FirstName = "Darlene", LastName = "Mullins", Age = 18, PhoneNumber = "503-232-1248" });
+            list.Add(new Contact() { FirstName = "Mary", LastName = "Calderon", Age = 20, PhoneNumber = "928-566-3280" });
+            list.Add(new Contact() { FirstName = "Robert", LastName = "Clark", Age = 27, PhoneNumber = "213-827-5284" });
+            list.Add(new Contact() { FirstName = "Leroy", LastName = "Vargas", Age = 50, PhoneNumber = "404-555-4250" });
+
+            secondTask.CallLinqMethods(list);
+        }
+    }
+}
