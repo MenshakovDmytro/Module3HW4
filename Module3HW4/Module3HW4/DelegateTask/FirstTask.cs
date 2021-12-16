7c4a05934cada5c47ef4a71b07f47b9c9bdd7eb0
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-16 21:20:08 +0200

    added delegate task

---
+++ Module3HW4/Module3HW4/DelegateTask/FirstTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Module3HW4.DelegateTask
+{
+    public class FirstTask
+    {
+        public event Func<double, double, double> SumEvent;
+
+        public double Sum(double x, double y) => x + y;
+
+        public double CountSum(double x, double y)
+        {
+            var sum = 0D;
+            SumEvent += Sum;
+            SumEvent += Sum;
+            foreach (Func<double, double, double> func in SumEvent.GetInvocationList())
+            {
+                sum += func.Invoke(x, y);
+            }
+
+            return sum;
+        }
+
+        public void TryCatch(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
