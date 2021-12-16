a97412ef4f1da7838c74873e5af800b8fee3bb34
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-16 21:20:16 +0200

    added Contact

---
+++ Module3HW4/Module3HW4/LinqTask/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace Module3HW4.LinqTask
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int Age { get; set; }
+    }
+}
